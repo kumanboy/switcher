1b0505ac3e137c2b605b105e3e7948ec2421c247
kumanboy <husandavronov13@gmail.com>
2025-03-12 15:43:41 +0500

    switcher end

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import Switcher from "./components/Switcher.tsx";
+import "./index.css"
+const App = () => {
+
+    return (
+        <div>
+            <Switcher/>
+        </div>
+    )
+}
+export default App
